df4efc3b339a5f420e3b22be36264a707098db93
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-01-19 12:34:44 +0300

    Доабвил рандомизацию труб по высоте и смерть игрока при выходе за границы

--- Assets/Scripts/Tune.cs
+++ Assets/Scripts/Tune.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Tune : MonoBehaviourExt
 {
     [SerializeField] private float _moveSpeed = 1.0f;
+    [SerializeField] private float _yPosRandInterval = 2.0f;
+
+    private void Start()
+    {
+        TransformPosY = Random.Range(TransformPosY - _yPosRandInterval, TransformPosY + _yPosRandInterval);
+    }
 
     private void Update()
     {
